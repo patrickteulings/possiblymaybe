bd0396fa8c0a3840ff02dd7c4fa0783a89362ba4
Patrick Teulings <patrickteulings@gmail.com>
2021-12-20 22:42:07 +0100

    Added socials and tweaked animations

--- template-parts/header/site-nav.php
+++ template-parts/header/site-nav.php
@@ -14,20 +14,30 @@
     <button class="navigation__trigger" name="Toggle menu" aria-label="Toggle menu" aria-controls="primary-menu-list" aria-expanded="false">
       <span></span><span></span><span></span>
     </button>
-    <div class="menu-container-wrapper">
-		<?php
-		wp_nav_menu(
-			array(
-				'theme_location'  => 'primary',
-				'menu_class'      => 'menu-wrapper',
-				'container_class' => 'primary-menu-container',
-				'items_wrap'      => '<ul id="primary-menu-list" class="%2$s">%3$s</ul>',
-				'fallback_cb'     => false,
-        'walker'          => new POSSIBLYMAYBE_Menu_Walker()
-			)
-		);
-		?>
-    <div class="socials-wrapper">fdsfs</div>
+    <div class="menu-container-wrapper is-open">
+      <div>
+        <?php
+        wp_nav_menu(
+          array(
+            'theme_location'  => 'primary',
+            'menu_class'      => 'menu-wrapper',
+            'container_class' => 'primary-menu-container',
+            'items_wrap'      => '<ul id="primary-menu-list" class="%2$s">%3$s</ul>',
+            'fallback_cb'     => false,
+            'walker'          => new POSSIBLYMAYBE_Menu_Walker()
+          )
+        );
+        ?>
+        <div class="socials-wrapper">
+          <div class="socials-wrapper__inner">
+            <ul class="socials">
+              <li class="socials__item linkedIn"><a href="https://www.linkedin.com/in/patrickteulings/" target="_blank" title="Visit PossiblyMaybe / Patrick Teulings on LinkedIn"><span class="socials__label">LinkedIn</span><i class="socials__icon"><?php possiblymaybe_get_icon('linkedin'); ?></i></a></li>
+              <li class="socials__item instagram"><a href="https://www.instagram.com/patrickteulings/" target="_blank"title="Visit PossiblyMaybe / Patrick Teulings on Instagram"><span class="socials__label">Instagram</span><i class="socials__icon"><?php possiblymaybe_get_icon('instagram'); ?></i></a></li>
+              <li class="socials__item twitter"><a href="https://twitter.com/patrickteulings" target="_blank"><span class="socials__label" title="Visit PossiblyMaybe / Patrick Teulings on Twitter">Twitter</span><i class="socials__icon"><?php possiblymaybe_get_icon('twitter'); ?></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
 	</nav><!-- #site-navigation -->
 <?php endif; ?>
